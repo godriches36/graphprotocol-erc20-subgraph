d62dc334a5c088a80711b4faf59cdf85e2604936
Black Sun God <godriches072@gmail.com>
2025-12-17 20:45:39 +0100

    Update and rename utils.ts to script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+async function fetchBlock() {
+
+    let displayBlock = document.getElementById("block-number")
+
+    // Fetch the latest block number querying the Etherscan API
+    const response = await fetch("/api")
+
+    // Place the JSON response in a constant and log it
+    const blockHex = await response.json()
+    console.log(`The latest block in HEX is: ${blockHex.result}`)
+
+    // Extract the HEX block value from the JSON
+    const blockString = JSON.stringify(blockHex.result)
+    //console.log(blockString)
+
+    // Slice the HEX value to remove 0x
+    const slicedBlock = blockString.slice(3, -1)
+    //console.log(slicedBlock)
+
+    // Convert and log the decimal block number
+    const blockDecimal = parseInt(slicedBlock, 16)
+    console.log(`The latest block number is: ${blockDecimal}`)
+    displayBlock.innerHTML = blockDecimal
+}
